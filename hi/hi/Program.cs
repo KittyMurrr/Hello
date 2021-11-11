329b740a98ec278cc07e2bb56c2c0c1a2612dc51
Александра Соловьева <kotyanya7000@mail.ru>
2021-11-11 16:17:44 +0700

    1 project

---
+++ hi/hi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace hi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Меню:");
+
+            Console.WriteLine("\ta - выполнить операцию «+»");
+            Console.WriteLine("\tb - выполнить операцию «-»");
+            Console.WriteLine("\tc - выполнить операцию «*»");
+            Console.WriteLine("\td - выполнить операцию «/»");
+
+            Console.WriteLine("Выберите, что вы хотите сделать!!!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
